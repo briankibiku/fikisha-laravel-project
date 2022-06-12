52785b993138ccb8059a2efb3b219f6919849e8d
kibiku <kibikukimani2@gmail.com>
2022-06-12 18:59:59 +0300

    update database

--- config/database.php
+++ config/database.php
@@ -45,7 +45,6 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'fikisha'),
